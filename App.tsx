2f43dbd51ae000e533d454f794509df2dd6748fe
a3.kozlova <a3.kozlova@cmdb-80003.itransition.corp>
2021-08-10 16:53:05 +0300

    fux bugs

--- App.tsx
+++ App.tsx
@@ -1,16 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   View,
+  Text,
+  StyleSheet,
 } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import './utils/i18n/index';
 
-const App: React.FC = () => <View />;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 20,
+  },
+  text: {
+    color: '#808080',
+  },
+});
+
+const App: React.FunctionComponent = () => {
+  const { t } = useTranslation();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        {t('title')}
+      </Text>
+    </View>
+  );
+};
 
 export default App;
