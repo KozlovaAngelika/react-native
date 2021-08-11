bc04a54f7f45684d2e651504173af73c3ae7f11d
a3.kozlova <a3.kozlova@cmdb-80003.itransition.corp>
2021-08-11 12:23:14 +0300

    configure redux

---
+++ src/redux/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+
+const initialState: MovieState = {
+  movies: [],
+  top250: [],
+  favorites: [],
+};
+
+const reducer = (
+  state: MovieState = initialState,
+) : MovieState => state;
+
+const state = createStore(reducer, initialState);
