d7fe7cd4b1e6c0671e41c4f8648818a3b5cc39bd
a3.kozlova <a3.kozlova@cmdb-80003.itransition.corp>
2021-08-11 10:09:55 +0300

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,10 @@ import React from 'react';
 import {
   View,
   Text,
-  StyleSheet,
 } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import './utils/i18n/index';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 20,
-  },
-  text: {
-    color: '#808080',
-  },
-});
+import styles from './styles';
 
 const App: React.FunctionComponent = () => {
   const { t } = useTranslation();
---
+++ src/styles.ts
@@ -0,0 +1,18 @@
+import {
+  StyleSheet,
+} from 'react-native';
+
+const lightGray = '#808080';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 20,
+  },
+  text: {
+    color: lightGray,
+  },
+});
+export default styles;
