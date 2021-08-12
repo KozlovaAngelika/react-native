aa30c9ac9c069da0145130eb2ca0b06ede3736aa
a3.kozlova <a3.kozlova@cmdb-80003.itransition.corp>
2021-08-12 13:08:41 +0300

    fix bugs

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-// import HomeIcon from '@material-ui/icons/Home';
-// import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-// import StarIcon from '@material-ui/icons/Star';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../../screens/Home';
 import TopMovies from '../../screens/TopMovies';
@@ -11,8 +8,13 @@ import Favorites from '../../screens/Favorites';
 const Tab = createMaterialBottomTabNavigator();
 
 const Tabs: React.FunctionComponent = () => (
-  // eslint-disable-next-line react-native/no-inline-styles
-  <Tab.Navigator initialRouteName="Home" barStyle={{ backgroundColor: '#d3d3d3' }}>
+  <Tab.Navigator
+    initialRouteName="Home"
+    // eslint-disable-next-line react-native/no-inline-styles
+    barStyle={{
+      backgroundColor: '#d3d3d3',
+    }}
+  >
     <Tab.Screen
       name="Home"
       component={Home}
