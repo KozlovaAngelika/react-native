2faa29246bb06d2085b953a6192b9462a12addca
a3.kozlova <a3.kozlova@cmdb-80003.itransition.corp>
2021-08-12 12:59:53 +0300

    add icons to bottom bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './utils/i18n/index';
+import 'react-native-vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './components/Tabs';
 
--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeIcon from '@material-ui/icons/Home';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import StarIcon from '@material-ui/icons/Star';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+// import HomeIcon from '@material-ui/icons/Home';
+// import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+// import StarIcon from '@material-ui/icons/Star';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../../screens/Home';
 import TopMovies from '../../screens/TopMovies';
 import Favorites from '../../screens/Favorites';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const Tabs: React.FunctionComponent = () => (
-  <Tab.Navigator initialRouteName="Home">
-    <Tab.Screen name="Home" component={Home} tabBarIcon={HomeIcon} />
-    <Tab.Screen name="TopMovies" component={TopMovies} tabBarIcon={ThumbUpAltIcon} />
-    <Tab.Screen name="Favorites" component={Favorites} tabBarIcon={StarIcon} />
+  // eslint-disable-next-line react-native/no-inline-styles
+  <Tab.Navigator initialRouteName="Home" barStyle={{ backgroundColor: '#d3d3d3' }}>
+    <Tab.Screen
+      name="Home"
+      component={Home}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name="home" size={26} color="#fff" />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="TopMovies"
+      component={TopMovies}
+      options={{
+        tabBarLabel: 'Top 250',
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name="thumb-up" size={26} color="#fff" />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Favorites"
+      component={Favorites}
+      options={{
+        tabBarLabel: 'Favorites',
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name="star" size={26} color="#fff" />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
