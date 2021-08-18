b5ef29537b1bbc1b07f38ef0fd7d8fe3d0f503d0
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-18 15:23:59 +0300

    added debounce

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import debounce from 'lodash.debounce';
 import { TextInput, View } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { useTranslation } from 'react-i18next';
@@ -14,13 +15,18 @@ interface Props {
 const SearchBar: React.FC<Props> = ({ value, onChangeValue }) => {
   const { t } = useTranslation();
   const placeholder = `${t('search')[0].toUpperCase()}${t('search').slice(1)}`;
+  const debouncedOnChangeValue = useCallback(
+    () => debounce(onChangeValue, 300),
+    [onChangeValue],
+  );
+
   return (
     <View style={styles.searchPanel}>
       <TextInput
         placeholder={placeholder}
         selectionColor={COLORS.BLACK}
         value={value}
-        onChangeText={onChangeValue}
+        onChangeText={debouncedOnChangeValue}
         style={styles.searchInput}
       />
       <Icon name="search" />
