914104c050391b8f7ea2517450e6a3a4c4f55902
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-18 10:01:12 +0300

    fixed bug

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import {
-  TextInput,
-  View,
-} from 'react-native';
+import { TextInput, View } from 'react-native';
 import { Icon } from 'react-native-elements';
 import styles from './styles';
 
 interface SearchBarProps {
-  value: string,
-  onChangeValue: (e: React.FormEvent) => void
+  value: string;
+  onChangeValue: (e: React.FormEvent) => void;
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ value, onChangeValue }) => (
   <View style={styles.searchPanel}>
-    <TextInput placeholder="Search" selectionColor="#808080" value={value} onChange={onChangeValue} style={styles.searchInput} />
+    <TextInput
+      placeholder="Search"
+      selectionColor="#808080"
+      value={value}
+      onChange={onChangeValue}
+      style={styles.searchInput}
+    />
     <Icon name="search" />
   </View>
 );
