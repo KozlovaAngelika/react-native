f2adc3e2456a32d52b2ef7bf473ff504acb2ea95
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-18 12:57:17 +0300

    added i18n for searchbar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { TextInput, View } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { useTranslation } from 'react-i18next';
 import { COLORS } from '../../utils/constants';
 import styles from './styles';
+import '../../utils/i18n';
 
 interface SearchBarProps {
   value: string;
   onChangeValue: (e: React.FormEvent) => void;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ value, onChangeValue }) => (
-  <View style={styles.searchPanel}>
-    <TextInput
-      placeholder="Search"
-      selectionColor={COLORS.BLACK}
-      value={value}
-      onChange={onChangeValue}
-      style={styles.searchInput}
-    />
-    <Icon name="search" />
-  </View>
-);
+const SearchBar: React.FC<SearchBarProps> = ({ value, onChangeValue }) => {
+  const { t } = useTranslation();
+  const placeholder = `${t('search')[0].toUpperCase()}${t('search').slice(1)}`;
+  return (
+    <View style={styles.searchPanel}>
+      <TextInput
+        placeholder={placeholder}
+        selectionColor={COLORS.BLACK}
+        value={value}
+        onChange={onChangeValue}
+        style={styles.searchInput}
+      />
+      <Icon name="search" />
+    </View>
+  );
+};
 
 export default SearchBar;
