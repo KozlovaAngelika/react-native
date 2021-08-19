083020ffd43e95b4ed5e2a80de8fcf32dbae7177
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-19 15:58:54 +0300

    Update index.tsx
    moved movie tile to home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
+import MovieTile from '../../components/MovieTile';
 import SearchBar from '../../components/SearchBar';
 
 const Home: React.FunctionComponent = () => {
@@ -7,6 +8,10 @@ const Home: React.FunctionComponent = () => {
   return (
     <View>
       <SearchBar value={searchValue} onChangeValue={setSearchValue} />
+      <MovieTile
+        title="Rick and Morty"
+        imgSrc="https://images.ua.prom.st/1650079983_w640_h640_1650079983.jpg"
+      />
     </View>
   );
 };
