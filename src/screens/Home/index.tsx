f262c01e568e4054991c1f9d63a07f4c53739e5b
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-19 16:26:57 +0300

    changed styles

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { View } from 'react-native';
 import MovieTile from '../../components/MovieTile';
 import SearchBar from '../../components/SearchBar';
+import styles from './styles';
 
 const Home: React.FunctionComponent = () => {
   const [searchValue, setSearchValue] = useState('');
   return (
-    <View>
+    <View style={styles.container}>
       <SearchBar value={searchValue} onChangeValue={setSearchValue} />
       <MovieTile
         title="Rick and Morty"
