5199442b16d3edfde3e7db45ec2dc8034f0ab156
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-19 11:56:57 +0300

    Update index.tsx
    moved searchbar to home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
+import SearchBar from '../../components/SearchBar';
 
-const Home: React.FunctionComponent = () => <View />;
+const Home: React.FunctionComponent = () => {
+  const [searchValue, setSearchValue] = useState('');
+  return (
+    <View>
+      <SearchBar value={searchValue} onChangeValue={setSearchValue} />
+    </View>
+  );
+};
 
 export default Home;
