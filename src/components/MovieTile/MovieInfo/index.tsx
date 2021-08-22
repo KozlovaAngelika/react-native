32909e2ffa821c641b1fc908571ab5967c7110e4
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-22 17:53:53 +0300

    implemented movie info component

---
+++ src/components/MovieTile/MovieInfo/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Button, Overlay, Card } from 'react-native-elements';
+import { useTranslation } from 'react-i18next';
+import styles from './styles';
+import { COLORS } from '../../../utils/constants';
+import '../../../utils/i18n';
+
+interface Props {
+  isModalVisible: boolean;
+  closeModal: () => void;
+  //   data: Movie[];
+}
+
+const MovieInfo: React.FC<Props> = ({ isModalVisible, closeModal }) => {
+  const { t } = useTranslation();
+  return (
+    <Overlay
+      isVisible={isModalVisible}
+      fullScreen
+      overlayStyle={styles.overlay}
+    >
+      <Button
+        icon={{
+          name: 'close',
+          size: 20,
+          color: `${COLORS.GREY}`,
+        }}
+        buttonStyle={styles.closeBtn}
+        containerStyle={styles.btnContainer}
+        onPress={closeModal}
+      />
+      <Card containerStyle={styles.card}>
+        <Card.Title>Title</Card.Title>
+        <Text style={styles.description}>
+          The idea with React Native Elements is more about component structure
+          than actual design.
+        </Text>
+        <View style={styles.ratingContainer}>
+          <Text style={styles.ratingTitle}>{t('rating')}</Text>
+          <Text>9.4</Text>
+        </View>
+        <Card.Image
+          source={{
+            uri:
+              'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/146b1b20-347b-4b6a-98c8-fdc2c75495cb/300x450',
+          }}
+          resizeMode="contain"
+        />
+      </Card>
+      <Button title={t('addtofavorites')} buttonStyle={styles.addBtn} />
+    </Overlay>
+  );
+};
+
+export default MovieInfo;
--- src/components/MovieTile/index.tsx
+++ src/components/MovieTile/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-native-elements';
+import MovieInfo from './MovieInfo';
+import styles from './MovieInfo/styles';
 
 interface Props {
   title: string;
   imgSrc: string;
 }
 
-const MovieTile: React.FC<Props> = ({ title, imgSrc }) => (
-  <Card>
-    <Card.Title>{title}</Card.Title>
-    <Card.Image
-      source={{
-        uri: imgSrc,
-      }}
-    />
-  </Card>
-);
+const MovieTile: React.FC<Props> = ({ title, imgSrc }) => {
+  const [isModalVisible, setIsModalVisible] = useState(true);
+  const closeModal = (): void => {
+    setIsModalVisible(false);
+  };
+  const showModal = (): void => {
+    setIsModalVisible(true);
+  };
+  return (
+    <Card>
+      <Card.Title onPress={showModal}>{title}</Card.Title>
+      <Card.Image
+        source={{
+          uri: imgSrc,
+        }}
+        resizeMode="contain"
+      />
+      <MovieInfo isModalVisible={isModalVisible} closeModal={closeModal} />
+    </Card>
+  );
+};
 
 export default MovieTile;
