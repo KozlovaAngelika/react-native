e6ed5dd483ce760a9b2fa2a1cb1e8d4f701586a1
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-23 10:28:07 +0300

    added scroll

--- src/components/MovieTile/MovieInfo/index.tsx
+++ src/components/MovieTile/MovieInfo/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { Button, Overlay, Card } from 'react-native-elements';
 import { useTranslation } from 'react-i18next';
 import styles from './styles';
@@ -30,23 +30,29 @@ const MovieInfo: React.FC<Props> = ({ isModalVisible, closeModal, data }) => {
         containerStyle={styles.btnContainer}
         onPress={closeModal}
       />
-      <Card containerStyle={styles.card}>
-        <Card.Title>{data.title}</Card.Title>
-        <Text style={styles.description}>
-          The idea with React Native Elements is more about component structure
-          than actual design.(найти откуда вытащить описание фильма)
-        </Text>
-        <View style={styles.ratingContainer}>
-          <Text style={styles.ratingTitle}>{t('rating')}</Text>
-          <Text>{data.imDbRating}</Text>
-        </View>
-        <Card.Image
-          source={{
-            uri: data.image,
-          }}
-          resizeMode="contain"
-        />
-      </Card>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Card containerStyle={styles.card}>
+          <Card.Title>{data.title}</Card.Title>
+          <Text style={styles.description}>
+            У сотрудника крупного банка всё идёт по накатанной, пока однажды он
+            не выясняет, что окружающий его мир — это часть огромной видеоигры,
+            а сам он в ней — всего лишь второстепенный персонаж. Хватит ли у
+            него духу переписать свой код, обратить на себя внимание прекрасной
+            девушки и, наконец, спасти мир? Одним словом, получится ли из него
+            главный герой?
+          </Text>
+          <View style={styles.ratingContainer}>
+            <Text style={styles.ratingTitle}>{t('rating')}</Text>
+            <Text>{data.imDbRating}</Text>
+          </View>
+          <Card.Image
+            source={{
+              uri: data.image,
+            }}
+            resizeMode="contain"
+          />
+        </Card>
+      </ScrollView>
       <Button title={t('addtofavorites')} buttonStyle={styles.addBtn} />
     </Overlay>
   );
