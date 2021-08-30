28f565eede884b2cbad0a6f503a2f396b190b7b9
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-30 17:05:17 +0300

    made corrections

--- src/screens/Favorites/styles.ts
+++ src/screens/Favorites/styles.ts
@@ -2,8 +2,6 @@ import { Platform, StatusBar, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     paddingHorizontal: 5,
   },
   moviesContainer: {
