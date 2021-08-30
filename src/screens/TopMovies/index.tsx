ebeb1538fc0adeb9c42aaecdeca04ef2bec219af
Angelika Kozlova <a3.kozlova@itransition.com>
2021-08-30 17:03:39 +0300

    made corrections

--- src/screens/TopMovies/index.tsx
+++ src/screens/TopMovies/index.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { SafeAreaView, FlatList, ListRenderItem } from 'react-native';
+import { View, FlatList, ListRenderItem } from 'react-native';
 import { useSelector } from 'react-redux';
 import MovieTile from '../../components/MovieTile';
 import styles from './styles';
 
 const TopMovies: React.FunctionComponent = () => {
   const data: Movie[] = useSelector((state: MovieState) => state.movies);
-  const keyExtractor = (item: Movie): string => item.id;
   const renderItem: ListRenderItem<Movie> = ({ item }): React.ReactElement => (
-    <MovieTile title={item.title} imgSrc={item.image} />
+    <MovieTile title={item.title} imgSrc={item.image} key={item.id} />
   );
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <FlatList
         data={data}
         renderItem={renderItem}
-        keyExtractor={keyExtractor}
         style={styles.moviesContainer}
       />
-    </SafeAreaView>
+    </View>
   );
 };
 
