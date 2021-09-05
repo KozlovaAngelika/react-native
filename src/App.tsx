f1004acfd028296e4790e553f5634640b88bfd52
Angelika Kozlova <a3.kozlova@itransition.com>
2021-09-05 16:42:32 +0300

    added theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'react-native-elements';
 import { Provider } from 'react-redux';
 import './utils/i18n/index';
 import 'react-native-vector-icons';
 import Tabs from './routes';
 import state from './redux';
+import theme from './theme';
 
 const App: React.FunctionComponent = () => (
-  <Provider store={state}>
-    <Tabs />
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <Provider store={state}>
+      <Tabs />
+    </Provider>
+  </ThemeProvider>
 );
 
 export default App;
