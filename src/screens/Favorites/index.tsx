35567b75cd58bfdea5e13ce34e8e0fa65e246f86
Angelika Kozlova <a3.kozlova@itransition.com>
2021-09-09 17:18:20 +0300

    added notification for favorites screen

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import Notice from 'components/Notice';
+import React, { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
 import { View, FlatList, ListRenderItem } from 'react-native';
 import { useSelector } from 'react-redux';
 import { selectMovies } from 'store/favorites/selectors';
@@ -6,19 +8,27 @@ import MovieTile from '../../components/MovieTile';
 import styles from './styles';
 
 const Favorites: React.FunctionComponent = () => {
+  const { t } = useTranslation();
   const data: Movie[] = useSelector(selectMovies);
+
   const renderItem: ListRenderItem<Movie> = ({ item }): React.ReactElement => (
     <MovieTile data={item} key={item.id} />
   );
-  return (
-    <View style={styles.container}>
+
+  const renderContent = (): ReactElement => {
+    if (!data.length) {
+      return <Notice isError={false} message={t('noFavorites')} />;
+    }
+    return (
       <FlatList
         data={data}
         renderItem={renderItem}
         style={styles.moviesContainer}
       />
-    </View>
-  );
+    );
+  };
+
+  return <View style={styles.container}>{renderContent()}</View>;
 };
 
 export default Favorites;
