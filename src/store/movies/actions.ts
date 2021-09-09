a05cf526d36c347790b6333a83802046ed693121
Angelika Kozlova <a3.kozlova@itransition.com>
2021-09-09 14:43:29 +0300

    added spaces

--- src/store/movies/actions.ts
+++ src/store/movies/actions.ts
@@ -12,17 +12,21 @@ import {
 export const searchMoviesStarted = (): SearchMovies => ({
   type: types.SEARCH_MOVIES_STARTED,
 });
+
 export const searchMoviesSuccess = (data: Movie[]): SearchMoviesSuccess => ({
   type: types.SEARCH_MOVIES_SUCCESS,
   payload: data,
 });
+
 export const searchMoviesFail = (error: Error | null): SearchMoviesFail => ({
   type: types.SEARCH_MOVIES_FAIL,
   payload: error,
 });
+
 export const clearSearchResults = (): ClearSearchResults => ({
   type: types.CLEAR_SEARCH_RESULTS,
 });
+
 export const searchMovies = (
   value: string,
 ): RootThunkAction<SearchMoviesActions> => (dispatch, getState, api) => {
