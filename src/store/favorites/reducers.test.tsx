b63e3cd4d4d7f95bc69100836898d311ad7c7b7a
KozlovaAngelika <kozlovaangelika09@gmail.com>
2021-09-27 12:12:37 +0300

    added tests for favorites reducers

---
+++ src/store/favorites/reducers.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import favoritesReducer from './reducers';
+import * as types from './actionTypes';
+
+let initialState: FavoritesState = {
+  data: [],
+};
+
+describe('favorites reducers', () => {
+  it('ADD_MOVIE_TO_FAVORITES', () => {
+    const action = {
+      type: types.ADD_MOVIE_TO_FAVORITES,
+      payload: {
+        id: 'test',
+        title: 'test',
+        image: 'test',
+      },
+    };
+    expect(favoritesReducer(initialState, action)).toEqual({
+      ...initialState,
+      data: [action.payload],
+    });
+  });
+
+  it('REMOVE_MOVIE_TO_FAVORITES', () => {
+    const action = {
+      type: types.REMOVE_MOVIE_FROM_FAVORITES,
+      payload: 'test',
+    };
+    initialState = {
+      data: [
+        {
+          id: 'test',
+          image: 'test',
+          title: 'test',
+        },
+      ],
+    };
+    expect(favoritesReducer(initialState, action)).toEqual({
+      ...initialState,
+      data: [],
+    });
+  });
+});
