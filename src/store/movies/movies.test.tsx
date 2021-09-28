95652374a3ebc0ee9cd179094dcf69e72c5f620a
KozlovaAngelika <kozlovaangelika09@gmail.com>
2021-09-28 12:53:38 +0300

    added tests for movies selectors

--- src/store/movies/movies.test.tsx
+++ src/store/movies/movies.test.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
-import { Provider } from 'react-redux';
-import Home from 'screens/Home';
-import thunk from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
+import { RootState } from 'store';
 import moviesReducer from './reducers';
 import * as types from './actionTypes';
+import { selectLoadingStatus, selectMovies, selectError } from './selectors';
 
 describe('movies reducer', () => {
   const initialState: MoviesState = {
@@ -68,6 +64,82 @@ describe('movies reducer', () => {
       loading: false,
       error: null,
       data: null,
+    });
+  });
+
+  describe('movies selectors', () => {
+    let state: RootState = {
+      movies: {
+        loading: false,
+        error: null,
+        data: [
+          {
+            id: 'firstMovie',
+            title: 'test',
+            image: 'test',
+          },
+          {
+            id: 'secondMovie',
+            title: 'test',
+            image: 'test',
+          },
+          {
+            id: 'thirdMovie',
+            title: 'test',
+            image: 'test',
+          },
+        ],
+      },
+      topMovies: {
+        loading: false,
+        error: null,
+        data: [],
+      },
+      favorites: {
+        data: [],
+      },
+    };
+
+    it('should return correct data', () => {
+      expect(selectMovies(state)).toEqual(state.movies.data);
+    });
+
+    it('should return correct error status', () => {
+      state = {
+        movies: {
+          loading: false,
+          error: new Error(),
+          data: [],
+        },
+        topMovies: {
+          loading: false,
+          error: null,
+          data: [],
+        },
+        favorites: {
+          data: [],
+        },
+      };
+      expect(selectError(state)).toEqual(state.movies.error);
+    });
+
+    it('should return correct loading status', () => {
+      state = {
+        movies: {
+          loading: true,
+          error: null,
+          data: null,
+        },
+        topMovies: {
+          loading: false,
+          error: null,
+          data: [],
+        },
+        favorites: {
+          data: [],
+        },
+      };
+      expect(selectLoadingStatus(state)).toEqual(state.movies.loading);
     });
   });
 });
