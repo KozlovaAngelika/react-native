97b462bf054ee8ac09e37169501709102804b6fe
KozlovaAngelika <a3.kozlova@itransition.com>
2021-11-16 12:34:52 +0300

    renamed LanguageSelection to LanguagePicker

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,4 +1,4 @@
-import LanguageSelection from 'components/LanguageSelection';
+import LanguageSelection from 'components/LanguagePicker';
 import React, { ReactElement } from 'react';
 import Notice from 'components/Notice';
 import { useTranslation } from 'react-i18next';
